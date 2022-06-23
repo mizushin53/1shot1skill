81bdbac700cd339e876447f9fbf9c5c660fb72eb
mizushin53 <taurus.nebula@gmail.com>
2022-06-23 23:46:31 +0900

    navigationの追加

--- header.php
+++ header.php
@@ -3,5 +3,12 @@
 <title>1SHOT1SKILL</title>
 </head>
 <body>
+
+<nav>
+    <li>
+  <ul>Twitter</ul>
+  <ul>GitHub</ul>
+</li>
+<nav>
 <div><?php bloginfo('name'); ?></div>
 <div><?php bloginfo('description'); ?></div>
